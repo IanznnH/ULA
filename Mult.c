f054cd4ff9760398994891775ab8d25844595ee8
IAN OLIVEIRA SILVA <ianoliveira@alu.ufc.br>
2025-02-09 12:14:16 -0300

    Mudei as variaveis

--- Mult.c
+++ Mult.c
@@ -1,6 +1,15 @@
 void ULA_MUL(int8_t *A, int8_t *Q, int8_t *M, int8_t *overflow) {
-    int16_t result = (int16_t)(*Q) * (int16_t)(*M);
-    *A = (int8_t)(result >> 8); 
-    *Q = (int8_t)(result & 0xFF); 
-    *overflow = (*A != 0 && *A != -1); 
+    int16_t result = (int16_t)(*Q) * (int16_t)(*M);//Multiplica os operadores de 8 bits
+    
+    int8_t upper_part = (int8_t)(result >> 8); //Parte mais alta dos bits
+    int8_t lower_part = (int8_t)(result & 0xFF);//Parte mais baixa dos bits
+
+    *A = upper_part;
+    *Q = lower_part;
+
+    if (upper_part != 0 && upper_part != -1) {//Verifica se tem overflow ou não
+        *overflow = 1; 
+    } else {
+        *overflow = 0; 
+    }
 }
